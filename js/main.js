f23075ffd586ac1b54ef6b27427d791d1f646f0c
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-11-07 23:15:49 +0100

    feat: conclusione esercizio

--- js/main.js
+++ js/main.js
@@ -20,12 +20,52 @@ const infoBiglietto = document.querySelector('#compra');
 const prezzoKm = 0.21;
 const scontoMinorenne = 20;
 const scontoOver65 = 40;
-const inputName = document.querySelector('.name');
-const inputKm = document.querySelector ('.km');
-const inputEta = document.querySelector ('.eta');
 
 
+document.querySelector('.btn-genera').addEventListener('click', function(){ 
+const inputName = document.getElementById('inputname').value;
+const inputKm = document.getElementById('km').value;
+const inputEta = document.getElementById('eta').value;
+const prezzo = (parseFloat(inputKm) * 0.21).toFixed(2);
+console.log(inputName);
+console.log(parseFloat(inputKm));
+console.log(inputEta);
+console.log(parseFloat(prezzo));
+document.getElementById('prezzo-finale').innerHTML = `${prezzo} €`;
+document.getElementById('offerta').innerHTML = 'Prezzo Standard'
+document.getElementById('passeggero-nome').innerHTML = `${inputName}`;
+document.getElementById('carrozza').innerHTML = Math.floor(Math.random() * (20 - 1)) + 1;
+document.getElementById('codice').innerHTML = Math.floor(Math.random() * (99999 - 1)) + 1;
 
+
+if (inputEta === 'Minorenne') {
+    const prezzoMinorenne = parseFloat(prezzo - prezzo / 100 * scontoMinorenne).toFixed(2);
+    document.getElementById('offerta').innerHTML = 'Prezzo Junior';
+    document.getElementById('prezzo-finale').innerHTML = `${prezzoMinorenne} €`;
+    console.log(parseFloat(prezzoMinorenne));
+}
+else if (inputEta === 'Over 65'){
+    const prezzoOver65 = (prezzo - prezzo / 100 * scontoOver65).toFixed(2);
+    document.getElementById('offerta').innerHTML = 'Prezzo Senior'
+    document.getElementById('prezzo-finale').innerHTML = `${prezzoOver65} €`;
+    console.log (parseFloat(prezzoOver65));
+}
+
+})
+
+document.querySelector('.btn-elimina').addEventListener('click', function(){ 
+const inputName = document.getElementById('inputname').value = '';
+const inputKm = document.getElementById('km').value = '';
+const inputEta = document.getElementById('eta').value = '';
+
+if (infoBiglietto.style.display === 'block') {
+        bigliettoCompilato.style.display = 'none'
+    }
+    else{
+        infoBiglietto.style.display = 'block'
+    }
+
+})
 
 
 bottone.addEventListener('click', function(){
@@ -44,13 +84,13 @@ const bigliettoCompilato = document.querySelector('#compila');
 
 bottoneGenera.addEventListener('click', function(){
     if (bigliettoCompilato.style.display === 'block') {
-        bigliettoCompilato.style.display = 'none'
     }
     else{
         bigliettoCompilato.style.display = 'block'
     }
 }
 )
+
 
 
 
